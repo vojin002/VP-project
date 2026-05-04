22b881310e8dada0307dd69620ea6dab871eeb65
giokovacevic <kovacevico012@gmail.com>
2026-05-04 15:14:39 +0200

    Implemented Proxy in WCFClient with Dispose pattern

--- WCFClient/Program.cs
+++ WCFClient/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WCFClient.Proxies;
 
 namespace WCFClient
 {
@@ -10,6 +12,23 @@ namespace WCFClient
     {
         static void Main(string[] args)
         {
+            using (ConsumptionProxy proxy = new ConsumptionProxy())
+            {
+                DailyConsumptionSample sample = new DailyConsumptionSample
+                {
+                    Date = DateTime.Now,
+                    TotalForecastMWh = 12,
+                    TotalActualMWh = 10,
+                    PeakTime = DateTime.Now,
+                    PeakActualMW = 5,
+                    CountryCode = "RS",
+                    RowIndex = 1
+                };
+
+                proxy.Send(sample);
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ WCFClient/Proxies/ConsumptionProxy.cs
@@ -0,0 +1,57 @@
+﻿using Common.Contracts;
+using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFClient.Proxies
+{
+    public class ConsumptionProxy : IDisposable
+    {
+        private IConsumptionService client;
+        private ICommunicationObject communicationObj;
+        private bool _disposed = false;
+
+        public ConsumptionProxy()
+        {
+            ChannelFactory<IConsumptionService> factory = new ChannelFactory<IConsumptionService>("WCFClient");
+            client = factory.CreateChannel();
+            communicationObj = (ICommunicationObject)client;
+        }
+
+        public void Send(DailyConsumptionSample sample)
+        {
+            client.PushSample(sample);
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+
+            try
+            {
+                if (communicationObj != null)
+                {
+                    if (communicationObj.State == CommunicationState.Faulted)
+                    {
+                        communicationObj.Abort();
+                    }
+                    else
+                    {
+                        communicationObj.Close();
+                    }
+                }
+            }
+            catch
+            {
+                communicationObj.Abort();
+            }
+
+            _disposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
